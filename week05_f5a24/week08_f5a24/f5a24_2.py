bb4cc2af729d31e01e894aa8e6ce146cbb0137c4
jiangligong <110546328+jiangligong@users.noreply.github.com>
2023-10-18 18:16:44 +0800

    Add files via upload

---
+++ week05_f5a24/week08_f5a24/f5a24_2.py
@@ -0,0 +1,18 @@
+print("f5a24 黎家暉")
+
+class Employee :
+    def __init__(self) :
+        self.cut_tree = 3
+
+class Andy(Employee) :
+    def __init__(self , get_gold) :
+        super().__init__()
+        self.get_gold = get_gold
+
+    def getDetials(self) :
+        print("===getDetals===")
+        print(f"tree:{self.cut_tree}")
+        print(f"gold:{self.get_gold}")
+
+andy = Andy(1)
+andy.getDetials()
